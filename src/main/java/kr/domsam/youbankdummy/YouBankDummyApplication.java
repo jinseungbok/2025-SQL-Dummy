f94d2101062a5bea83a837d81485e8f1a05323a1
sbsteacher <id@gmail.com>
2025-11-21 15:28:27 +0900

    first

---
+++ src/main/java/kr/domsam/youbankdummy/YouBankDummyApplication.java
@@ -0,0 +1,13 @@
+package kr.domsam.youbankdummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouBankDummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouBankDummyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/domsam/youbankdummy/YouBankDummyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.domsam.youbankdummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouBankDummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
